73cecb519454e7cb51db2c7aacaf996eeee72d7f
Philip Durbin <philipdurbin@gmail.com>
2012-05-21 23:07:27 -0400

    use Git; other tweaks

--- bin/grgit.pl
+++ bin/grgit.pl
@@ -2,12 +2,12 @@
 use strict;
 use warnings;
 use 5.010000;
-use LWP::Simple qw{get};
 use Readonly;
 use Carp;
-use Data::Dumper;
-use IPC::Open3 'open3';
+use LWP::Simple qw{get};
+use Git;
 use YAML;
+#use Data::Dumper;
 
 Readonly my $GIT_SERVER     => 'git.greptilian.com';
 Readonly my $GIT_USER       => 'pdurbin';
@@ -17,8 +17,7 @@ Readonly my $LOCAL_GIT_DIR  => "$ENV{HOME}/gr";
 Readonly my $GIT_CLONE_PATH => "$GIT_SERVER:$PROJECT_DIR";
 Readonly my $DOTDOT         => q{..};
 Readonly my $FILES_NON_DOT  => q{*};
-Readonly my $DESCRIPTIONS =>
-"http://$GIT_SERVER/?p=wiki.git;a=blob_plain;f=greptilian.com/git/repos.mdwn;hb=HEAD";
+Readonly my $DESCRIPTIONS   => "http://$GIT_SERVER/?p=wiki.git;a=blob_plain;f=greptilian.com/git/repos.mdwn;hb=HEAD";
 
 chdir($LOCAL_GIT_DIR) or croak "Couldn't cd to $LOCAL_GIT_DIR";
 
@@ -38,26 +37,21 @@ if ( !$descriptions_yaml ) {
 }
 
 my $proj_descriptions = Load($descriptions_yaml);
-for my $repo ( keys %{$proj_descriptions} ) {
+
+for my $repo ( sort keys %{$proj_descriptions} ) {
     carp "No description for $repo at $DESCRIPTIONS" unless ($repo ~~ @projects);
 }
 
-for my $project_bare (@projects) {
+for my $project_bare (sort @projects) {
     carp "No description for $project_bare at $DESCRIPTIONS" unless ${$proj_descriptions}{$project_bare};
     my ($project_local) = $project_bare =~ /^(.*?)[.]git/;
     if ( chdir($project_local) ) {
-        printf( 'cd %-30s', "$project_local... " );
-        #say glob($FILES_NON_DOT);
-        my ( $writer, $reader, $err );
-        open3( $writer, $reader, $err, 'git pull' );
-        while ( my $line = <$reader> ) {
-            print {*STDOUT} $line
-              unless $line =~ /POSSIBLE BREAK-IN ATTEMPT/;
-        }
+        printf( '%-30s', "$project_local... " );
+        git_cmd_try { Git::command_noisy('pull') } '%s failed w/ code %d';
         chdir($DOTDOT);
     }
     else {
         print
-"Could not cd to $project_local. Clone with:\ngit clone $GIT_USER\@$GIT_CLONE_PATH/$project_local.git\n";
+"Could not cd to $project_local. Clone with:\ngit clone $GIT_USER\@$GIT_CLONE_PATH/$project_bare\n";
     }
 }
